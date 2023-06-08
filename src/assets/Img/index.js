4eb8f0f1aae5739dec36ce12657833bb623c1c1c
Kidus <98009847+kid-us@users.noreply.github.com>
2023-06-08 15:57:06 +0300

    Add files via upload

---
+++ src/assets/Img/index.js
@@ -0,0 +1,109 @@
+import abstract from './abstract.jpg';
+import abuse from './abuse.jpg';
+import boy1 from './boy1.jpg';
+import competion from './competion.jpg';
+import couples from './couples.jpg';
+import model from './model.jpg';
+import jesus from './jesus.jpg';
+import sinking from './sinking.jpg';
+import girl1 from './girl1.jpg';
+import girl2 from './girl2.jpg';
+import girl3 from './girl3.jpg';
+import girl4 from './girl4.jpg';
+import girl5 from './girl5.jpg';
+import girl6 from './girl6.jpg';
+import girl7 from './girl7.jpg';
+import girl8 from './girl8.jpg';
+import girl9 from './girl9.jpg';
+import girl10 from './girl10.jpg';
+import girl11 from './girl11.jpg';
+import girl12 from './girl12.jpg';
+import beauty1 from './beauty1.jpg';
+import Brownhairor from './Brownhairor.jpg';
+import Brownhair0 from './brownhair0.jpg';
+import Brownhair1 from './brownhair1.jpg';
+import coolor from './coolor.jpg';
+import couplesor from './couplesor.jpg';
+import couples0 from './couples0.jpg';
+import cute1 from './cutee1.jpg';
+import Cuteeor from './Cuteeor.jpg';
+import frombed0 from './frombed0.jpg';
+import good0 from './good0.jpg';
+import habeshaor from './habeshaor.jpg';
+import habesha1 from './habesha1.jpg';
+import Longhairor from './Longhairor.jpg';
+import long0 from './long0.jpg';
+import looking0 from './looking0.jpg';
+import Lookor from './Lookor.jpg';
+import photolike0 from './photolike0.jpg';
+import pissor from './pissor.jpg';
+import piss0 from './piss0.jpg';
+import sad0 from './sad0.jpg';
+import sad1 from './sad1.jpg';
+import Thinkingor from './Thinkingor.jpg';
+import thinking0 from './thinking0.jpg';
+import glass0 from './glass0.jpg';
+import photolikeor from './photolikeor.jpg';
+import goodor from './goodor.jpg';
+import beautyor from './beautyor.jpg';
+import glassor from './glassor.jpg';
+import jesus0 from './jesus0.jpg';
+import frombdeor from './frombedor.jpg';
+import perfector from './perfector.jpg';
+import cool0 from './cool0.jpg';
+
+export {
+    abstract,
+    abuse,
+    boy1,
+    competion,
+    couples,
+    model,
+    jesus,
+    sinking,
+    girl1,
+    girl2,
+    girl3,
+    girl4,
+    girl5,
+    girl6,
+    girl7,
+    girl8,
+    girl9,
+    girl10,
+    girl11,
+    girl12,
+    beautyor,
+    beauty1,
+    Brownhairor,
+    Brownhair0,
+    Brownhair1,
+    coolor,
+    couplesor,
+    couples0,
+    cute1,
+    Cuteeor,
+    frombed0,
+    glassor,
+    good0,
+    habesha1,
+    habeshaor,
+    Longhairor,
+    long0,
+    looking0,
+    Lookor,
+    photolike0,
+    pissor,
+    piss0,
+    sad0,
+    sad1,
+    Thinkingor,
+    thinking0,
+    photolikeor,
+    goodor,
+    glass0,
+    cool0,
+    perfector,
+    frombdeor,
+    jesus0
+};

e663a7b22219655fea167ebc061919b1e436dc1c
Kidus <98009847+kid-us@users.noreply.github.com>
2023-09-25 11:05:43 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,22 +11,7 @@ class App extends Component {
       <>
         <div className='container-fluid'>
           <Navbar />
-
-          <div className='my-name fw-semibold d-none d-md-block' title='Kidus'>
-            <a href="https://kidus-w.web.app" className='small'>
-              <ul>
-                <li className='py-3 code'> {'</>'} </li>
-                <br />
-                <li>K</li>
-                <li>i</li>
-                <li>d</li>
-                <li>u</li>
-                <li>s</li>
-              </ul>
-
-            </a>
-          </div>
-
+          
           <div className="row">
             <LeftSideBar />
           </div>
